5ea3c1a7ff142244ab0231500b69ec3e0a5e2398
Kody Simpson <kodysimpson0616@gmail.com>
2021-06-23 15:49:06 -0500

    main method docs

--- src/me/kodysimpson/Main.java
+++ src/me/kodysimpson/Main.java
@@ -1,13 +1,21 @@
 package me.kodysimpson;
 
 import me.kodysimpson.service.RestaurantService;
+import me.kodysimpson.service.di.MessageService;
+import me.kodysimpson.service.di.SpanishMessageServiceImpl;
+import me.kodysimpson.service.di.TextingService;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        //No dependency injection is used here, the FoodService is made within the RestaurantService
         RestaurantService restaurantService = new RestaurantService();
         restaurantService.processOrder();
+
+        //Example of manual constructor dependency injection
+        MessageService messageService = new SpanishMessageServiceImpl(); //we can choose any of the messageservice implementations
+        TextingService textingService = new TextingService(messageService); //inject it in!
 
     }
 }
